7c47297d18220208b2cbe3f79042129a67e1a35c
y-k17 <a.syadukrisnan2002@gmail.com>
2025-04-09 01:00:32 +0530

    Spinner Service Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { BackgroundComponent } from "./background/background.component";
 import { NavbarComponent } from './navbar/navbar.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, BackgroundComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, BackgroundComponent, NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { AuthInterceptor } from './auth.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), provideClientHydration(),
-    provideHttpClient(withInterceptors([AuthInterceptor]))
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
+    importProvidersFrom(
+      NgxSpinnerModule,
+      BrowserAnimationsModule
+    )
   ]
 };
