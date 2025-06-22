31e624ee910ee24415cccf001cc302565266b70b
y-k17 <a.syadukrisnan2002@gmail.com>
2025-06-22 17:17:13 +0530

    Landing page v1

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InViewportDirective } from '../directives/in-viewport.directive';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, RouterLink, ButtonModule,InViewportDirective],
+  imports: [CommonModule, ButtonModule,InViewportDirective],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent{
+
+  constructor(private router:Router,private spinner:NgxSpinnerService,public authService: AuthService) {}
 
   sectionVisible = {
     hero: false,
@@ -26,9 +30,16 @@ export class LandingPageComponent{
     this.sectionVisible[section] = event;
   }
 
+  login(){
+    this.authService.login();
+  }
 
   ngOnInit() {
-    // Any initialization logic
+    this.spinner.show();
+    this.authService.isAuthenticated$.subscribe(auth =>{
+      // this.router.navigate(['/home']);
+    })
+    this.spinner.hide();
   }
 
 }
